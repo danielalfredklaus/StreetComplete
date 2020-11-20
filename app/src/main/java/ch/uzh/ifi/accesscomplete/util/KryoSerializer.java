273272afb529205f0ac86ec299c715ea0c343d2a
svstoll <s.stoll@posteo.de>
2020-11-20 05:16:36 +0100

    Add explicit license headers to all files

--- app/src/main/java/ch/uzh/ifi/accesscomplete/util/KryoSerializer.java
+++ app/src/main/java/ch/uzh/ifi/accesscomplete/util/KryoSerializer.java
@@ -1,3 +1,25 @@
+/*
+ * AccessComplete, an easy to use editor of accessibility related
+ * OpenStreetMap data for Android.  This program is a fork of
+ * StreetComplete (https://github.com/westnordost/StreetComplete).
+ *
+ * Copyright (C) 2016-2020 Tobias Zwick and contributors (StreetComplete authors)
+ * Copyright (C) 2020 Sven Stoll (AccessComplete author)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package ch.uzh.ifi.accesscomplete.util;
 
 import com.esotericsoftware.kryo.Kryo;
@@ -27,37 +49,35 @@ import ch.uzh.ifi.accesscomplete.data.osm.splitway.SplitAtPoint;
 import ch.uzh.ifi.accesscomplete.quests.LocalizedName;
 
 @Singleton
-public class KryoSerializer implements Serializer
-{
+public class KryoSerializer implements Serializer {
 	// NEVER CHANGE THE ORDER OF THIS LIST. ALWAYS APPEND NEW CLASSES AT THE BOTTOM
 	// IF CLASSES ARE DELETED, INSERT A PLACEHOLDER (i.e. Object.class) THERE
 	private static final Class[] registeredClasses =
-	{
-			HashMap.class,
-			ArrayList.class,
-			Fixed1E7LatLon.class,
-			Element.Type.class,
-			OsmRelationMember.class,
-			StringMapChanges.class,
-			StringMapEntryAdd.class,
-			StringMapEntryDelete.class,
-			StringMapEntryModify.class,
-			NoteComment.class,
-			NoteComment.Action.class,
-			Date.class,
-			User.class,
-			boolean[].class,
-			LocalizedName.class,
-			OsmLatLon.class,
-			SplitAtPoint.class,
-			SplitAtLinePosition.class,
-	};
+			{
+					HashMap.class,
+					ArrayList.class,
+					Fixed1E7LatLon.class,
+					Element.Type.class,
+					OsmRelationMember.class,
+					StringMapChanges.class,
+					StringMapEntryAdd.class,
+					StringMapEntryDelete.class,
+					StringMapEntryModify.class,
+					NoteComment.class,
+					NoteComment.Action.class,
+					Date.class,
+					User.class,
+					boolean[].class,
+					LocalizedName.class,
+					OsmLatLon.class,
+					SplitAtPoint.class,
+					SplitAtLinePosition.class,
+			};
 
 
-	private static final ThreadLocal<Kryo> kryo = new ThreadLocal<Kryo>()
-	{
-		@Override protected Kryo initialValue()
-		{
+	private static final ThreadLocal<Kryo> kryo = new ThreadLocal<Kryo>() {
+		@Override
+		protected Kryo initialValue() {
 			Kryo kryo = new Kryo();
 
 			/* Kryo docs say that classes that are registered are serialized more space efficiently
@@ -65,27 +85,24 @@ public class KryoSerializer implements Serializer
 	 		   it is better) */
 			kryo.setRegistrationRequired(true);
 			kryo.setInstantiatorStrategy(new Kryo.DefaultInstantiatorStrategy(new StdInstantiatorStrategy()));
-			for(Class reg : registeredClasses)
-			{
+			for (Class reg : registeredClasses) {
 				kryo.register(reg);
 			}
 			return kryo;
 		}
 	};
 
-	@Override public byte[] toBytes(Object object)
-	{
-		try (Output output = new Output(1024, -1))
-		{
+	@Override
+	public byte[] toBytes(Object object) {
+		try (Output output = new Output(1024, -1)) {
 			kryo.get().writeObject(output, object);
 			return output.toBytes();
 		}
 	}
 
-	@Override public <T> T toObject(byte[] bytes, Class<T> type)
-	{
-		try (Input input = new Input(bytes))
-		{
+	@Override
+	public <T> T toObject(byte[] bytes, Class<T> type) {
+		try (Input input = new Input(bytes)) {
 			return kryo.get().readObject(input, type);
 		}
 	}
